f0d4296f19c55325cf747f002535eeee8acafa76
Zoltan Hricz <hriczzoli@gmail.com>
2019-08-04 15:00:15 +0100

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
 import firebase from 'firebase';
 let config = {
-    apiKey: "AIzaSyBztr_d542Qo4xJ7HmJDHAgbw7gQfr5UmI",
-    authDomain: "midwifevuepwa.firebaseapp.com",
-    databaseURL: "https://midwifevuepwa.firebaseio.com",
-    projectId: "midwifevuepwa",
-    storageBucket: "",
-    messagingSenderId: "1048086264064"
+    //your config details here
   };
 
   firebase.initializeApp(config);
