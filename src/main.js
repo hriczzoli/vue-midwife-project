58fba7ae5ec91997d957970f043994b3f2b86662
Zoltan Hricz <hriczzoli@gmail.com>
2019-08-04 15:10:28 +0100

    update main.js

--- src/main.js
+++ src/main.js
@@ -12,12 +12,7 @@ Vue.config.productionTip = false
 
 let app;
 // let config = {
-//   apiKey: "AIzaSyBztr_d542Qo4xJ7HmJDHAgbw7gQfr5UmI",
-//   authDomain: "midwifevuepwa.firebaseapp.com",
-//   databaseURL: "https://midwifevuepwa.firebaseio.com",
-//   projectId: "midwifevuepwa",
-//   storageBucket: "",
-//   messagingSenderId: "1048086264064"
+//   your config data
 // };
 // firebase.initializeApp(config);
 
